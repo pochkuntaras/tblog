70b8dddeeddac706af837a18f14b87b5723f3033
Taras Pochkun <pochkun_taras@mail.ru>
2017-01-14 11:06:53 +0200

    Add like button. (#2)

--- blog_item.js
+++ blog_item.js
@@ -1,7 +1,49 @@
-const BlogItem = ({ image, text }) => (
+const BlogItem = ({ image, text, meta }) => (
   <div>
     <Image {...image}/>
-    <TextBox text={text}/>
+    <ul>
+      <li><TextBox text={text}/></li>
+      <li><TextBox text={meta.author}/></li>
+      <li><TextBox text={dateFormat(meta.createdAt, 'DD MMM HH:mm')}/></li>
+      <li><TextBox text={dateFormat(meta.updatedAt, 'DD MMM HH:mm')}/></li>
+    </ul>      
+    <Like likes={meta.likes}/>
   </div>
 );
 
+BlogItem.defaultProps = {
+  meta: {
+    author: 'Username',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    likes: 0
+  },
+  image: {
+    alt: 'Image',
+    width: '100px',
+    height: '100px'
+  },
+  text: 'New post'
+};
+
+BlogItem.propTypes = {
+  meta: React.PropTypes.shape({
+    author: React.PropTypes.string.isRequired,
+    createdAt: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.instanceOf(Date)
+    ]),
+    updatedAt: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.instanceOf(Date)
+    ]),
+    likes: PropTypes.number
+  }),
+  image: React.PropTypes.shape({
+    src: React.PropTypes.string,
+    alt: React.PropTypes.string.isRequired,
+    width: React.PropTypes.string,
+    height: React.PropTypes.string
+  }),
+  text: React.PropTypes.string.isRequired
+};
--- blog_list.js
+++ blog_list.js
@@ -2,9 +2,51 @@ const BlogList = ({ posts }) => (
   <div>
   {
     _.map(posts, (post, key) => (
-      <BlogItem key={key} image={post.image} text={post.text}/>
+      <BlogItem key={key} {...post}/>
     ))
   }
   </div>
 );
 
+BlogList.defaultProps = {
+  posts: {
+    meta: {
+      author: 'Username',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      likes: 0
+    },
+    image: {
+      alt: 'Image',
+      width: '100px',
+      height: '100px'
+    },
+    text: 'New post'
+  }
+};
+
+BlogList.propTypes = {
+  posts: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      meta: React.PropTypes.shape({
+        author: React.PropTypes.string.isRequired,
+        createdAt: React.PropTypes.oneOfType([
+          React.PropTypes.string,
+          React.PropTypes.instanceOf(Date)
+        ]),
+        updatedAt: React.PropTypes.oneOfType([
+          React.PropTypes.string,
+          React.PropTypes.instanceOf(Date)
+        ]),
+        likes: PropTypes.number
+      }),
+      image: React.PropTypes.shape({
+        src: React.PropTypes.string,
+        alt: React.PropTypes.string.isRequired,
+        width: React.PropTypes.string,
+        height: React.PropTypes.string
+      }),
+      text: React.PropTypes.string.isRequired
+    })
+  )
+};
